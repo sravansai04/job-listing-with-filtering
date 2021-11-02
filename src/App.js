6136137ce4fa116987f3b9fe7cc5ef13a7ac8c5e
NALLA SRAVAN SAI RAHUL <sravansai.nalla@agilesolutions.com>
2021-11-02 22:39:16 +0530

    Job Listing using filtering

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import data from "./data.json";
+import Jobs from "./components/Jobs";
+import { useState } from "react";
+import Header from "./components/Header";
+import Search from "./components/Search";
+
+function App() {
+  const [filterKeywords, setfilterKeywords] = useState([]);
+  
+  const addFilterKeywords = (data) => {
+    if (!filterKeywords.includes(data)) {
+      setfilterKeywords([...filterKeywords, data]);
+    }
+  };
+
+  const deleteKeyword = (data) => {
+    const newKeywords = filterKeywords.filter((key) => key !== data);
+    setfilterKeywords(newKeywords);
+  };
+
+  const clearAll = () => {
+    setfilterKeywords([]);
+  };
+
+  return (
+    <div>
+      <div className="header"></div>
+
+      {/* <Search setSearchKeyword={setSearchKeyword} /> */}
+
+      {filterKeywords.length > 0 && (
+        <Header
+          keywords={filterKeywords}
+          removeKeywords={deleteKeyword}
+          clearAll={clearAll}
+        />
+      )}
+
+      <Jobs
+        keywords={filterKeywords}
+        data={data}
+        setKeywords={addFilterKeywords}
+      />
+    </div>
+  );
+}
+
+export default App;
